d71c3e89744ca95b26ada372a0a312f4248b66e0
sep-saikumar <emailgalamsaikumar585@gmail.com>
2025-02-07 21:27:15 +0530

    all topic shell script

---
+++ loops.sh
@@ -0,0 +1,14 @@
+a=10
+
+while [ &a - gt 0 ]; do
+  echo a - $a -greater than 0
+  a=$ (($a-1))
+  done
+
+
+  If expression is true than loop allows inside
+
+  for fruit in apple banana orange ; do
+    echo Fruit Name  - $ fruit
+    sleep |
+    done
